969a9eac9018d3efea2851a678022065c7a0d083
hasanguloglu <veyismir@hotmail.com>
2024-05-28 11:25:15 +0300

    customer window is done

---
+++ customer.py
@@ -0,0 +1,194 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk 
+
+class Custom_Window:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Hotel Management System")
+        self.root.geometry("1020x500+255+150")
+        
+        # ================= title =====================
+        label_title = Label(self.root, text="ADD CUSTOMER DETAILS", justify="center", font=("TkDefaultFont", 20, "bold"), bg= "dark blue", fg="white", bd=4, relief=RIDGE,)
+        label_title.place(x=0, y=0, width=1020, height=50)
+
+        # ================= logo =====================
+        img2 = Image.open(r"C:\\Users\\Nazife\\Desktop\\hotel_management_system\\images\\hotel_logo.jpg")
+        img2 = img2.resize((105, 45))
+        self.photoimg2 = ImageTk.PhotoImage(img2)
+
+        labelimg = Label(self.root, image = self.photoimg2, bd = 0, relief = RIDGE)
+        labelimg.place(x = 2, y = 2, width= 105, height= 45)
+
+        # ================= labelFrame =====================
+        label_frame_left = LabelFrame(self.root, text="Customer Details", padx=2, font=("TkDefaultFont", 10, "bold"), bd=4, relief=RIDGE,)
+        label_frame_left.place(x= 2, y=50, width=250, height=450)
+
+        # ================= labels and entries =====================
+        # Customer REF
+        label_customer_ref = Label(label_frame_left, text="Customer ID", font=("TkDefaultFont", 10, "bold"), padx=2, pady=6)
+        label_customer_ref.grid(row=0, column=0, sticky=W)
+
+        entry_ref = ttk.Entry(label_frame_left, width=10, font=("TkDefaultFont", 10, "bold"))
+        entry_ref.grid(row=0, column=1)
+
+        # Customer Name
+        customer_name = Label(label_frame_left, text="Customer Name", font=("TkDefaultFont", 10, "bold"), padx=2, pady=6)
+        customer_name.grid(row=1, column=0, sticky=W)
+
+        entry_customer_name = ttk.Entry(label_frame_left, width=10, font=("TkDefaultFont", 10, "bold"))
+        entry_customer_name.grid(row=1, column=1)
+
+        # Mother Name
+        mother_name = Label(label_frame_left, text="Mother Name", font=("TkDefaultFont", 10, "bold"), padx=2, pady=6)
+        mother_name.grid(row=2, column=0, sticky=W)
+
+        entry_mother_name = ttk.Entry(label_frame_left, width=10, font=("TkDefaultFont", 10, "bold"))
+        entry_mother_name.grid(row=2, column=1)
+
+        # Gender Dropdown
+        gender = Label(label_frame_left, text="Gender", font=("TkDefaultFont", 10, "bold"), padx=2, pady=6)
+        gender.grid(row=3, column=0, sticky=W)
+        combo_gender = ttk.Combobox(label_frame_left, font=("TkDefaultFont", 10, "bold"), width=8, state="readonly")
+        combo_gender["value"] = ("Male", "Female", "Other")
+        combo_gender.current(0)
+        combo_gender.grid(row=3, column=1)
+
+
+
+        # Postcode
+        postcode = Label(label_frame_left, text="Postcode", font=("TkDefaultFont", 10, "bold"), padx=2, pady=6)
+        postcode.grid(row=4, column=0, sticky=W)
+
+        entry_postcode = ttk.Entry(label_frame_left, width=10, font=("TkDefaultFont", 10, "bold"))
+        entry_postcode.grid(row=4, column=1)
+
+        # Mobile Number
+        mobile = Label(label_frame_left, text="Mobile", font=("TkDefaultFont", 10, "bold"), padx=2, pady=6)
+        mobile.grid(row=5, column=0, sticky=W)
+
+        entry_mobile = ttk.Entry(label_frame_left, width=10, font=("TkDefaultFont", 10, "bold"))
+        entry_mobile.grid(row=5, column=1)
+
+        # email 
+        email = Label(label_frame_left, text="Email", font=("TkDefaultFont", 10, "bold"), padx=2, pady=6)
+        email.grid(row=6, column=0, sticky=W)
+
+        entry_email = ttk.Entry(label_frame_left, width=10, font=("TkDefaultFont", 10, "bold"))
+        entry_email.grid(row=6, column=1)
+
+        # Nationality
+        nationality = Label(label_frame_left, text="Nationality", font=("TkDefaultFont", 10, "bold"), padx=2, pady=6)
+        nationality.grid(row=7, column=0, sticky=W)
+
+        combo_nationality = ttk.Combobox(label_frame_left, font=("TkDefaultFont", 10, "bold"), width=8, state="readonly")
+        combo_nationality["value"] = ('Afghan', 'Albanian', 'Algerian', 'American', 'Andorran', 'Angolan', 'Antiguans', 'Argentinean', 'Armenian', 'Australian', 'Austrian', 'Azerbaijani', 'Bahamian', 'Bahraini', 'Bangladeshi', 'Barbadian', 'Barbudans', 'Batswana', 'Belarusian', 'Belgian', 'Belizean', 'Beninese', 'Bhutanese', 'Bolivian', 'Bosnian', 'Brazilian', 'British', 'Bruneian', 'Bulgarian', 'Burkinabe', 'Burmese', 'Burundian', 'Cambodian', 'Cameroonian', 'Canadian', 'Cape Verdean', 'Central African', 'Chadian', 'Chilean', 'Chinese', 'Colombian', 'Comoran',  'Congolese', 'Costa Rican', 'Croatian', 'Cuban', 'Cypriot', 'Czech', 'Danish', 'Djibouti', 'Dominican', 'Dutch', 'Dutchman', 'Dutchwoman', 'East Timorese', 'Ecuadorean', 'Egyptian', 'Emirian', 'Equatorial Guinean', 'Eritrean', 'Estonian', 'Ethiopian', 'Fijian', 'Filipino', 'Finnish', 'French', 'Gabonese', 'Gambian', 'Georgian', 'German', 'Ghanaian', 'Greek', 'Grenadian', 'Guatemalan', 'Guinea-Bissauan', 'Guinean', 'Guyanese', 'Haitian', 'Herzegovinian', 'Honduran', 'Hungarian', 'I-Kiribati', 'Icelander', 'Indian', 'Indonesian', 'Iranian', 'Iraqi', 'Irish', 'Israeli', 'Italian', 'Ivorian', 'Jamaican', 'Japanese', 'Jordanian', 'Kazakhstani', 'Kenyan', 'Kittian and Nevisian', 'Kuwaiti', 'Kyrgyz', 'Laotian', 'Latvian', 'Lebanese', 'Liberian', 'Libyan', 'Liechtensteiner', 'Lithuanian', 'Luxembourger', 'Macedonian', 'Malagasy', 'Malawian', 'Malaysian', 'Maldivan', 'Malian', 'Maltese', 'Marshallese', 'Mauritanian', 'Mauritian', 'Mexican', 'Micronesian', 'Moldovan', 'Monacan', 'Mongolian', 'Moroccan', 'Mosotho', 'Motswana', 'Mozambican', 'Namibian', 'Nauruan', 'Nepalese', 'Netherlander', 'New Zealander', 'Ni-Vanuatu', 'Nicaraguan', 'Nigerian', 'Nigerien', 'North Korean', 'Northern Irish', 'Norwegian', 'Omani', 'Pakistani', 'Palauan', 'Panamanian', 'Papua New Guinean', 'Paraguayan', 'Peruvian', 'Polish', 'Portuguese', 'Qatari', 'Romanian', 'Russian', 'Rwandan', 'Saint Lucian', 'Salvadoran', 'Samoan', 'San Marinese', 'Sao Tomean', 'Saudi', 'Scottish', 'Senegalese', 'Serbian', 'Seychellois', 'Sierra Leonean', 'Singaporean', 'Slovakian', 'Slovenian', 'Solomon Islander', 'Somali', 'South African', 'South Korean', 'Spanish', 'Sri Lankan', 'Sudanese', 'Surinamer', 'Swazi', 'Swedish', 'Swiss', 'Syrian', 'Taiwanese', 'Tajik', 'Tanzanian', 'Thai', 'Togolese', 'Tongan', 'Trinidadian or Tobagonian', 'Tunisian', 'Turkish', 'Tuvaluan', 'Ugandan', 'Ukrainian', 'Uruguayan', 'Uzbekistani', 'Venezuelan', 'Vietnamese', 'Welsh', 'Yemenite', 'Zambian', 'Zimbabwean')
+        combo_nationality.current(0)
+        combo_nationality.grid(row=7, column=1)
+
+        # idproof type dropdown
+        id_proof = Label(label_frame_left, text="ID Proof Type", font=("TkDefaultFont", 10, "bold"), padx=2, pady=6)
+        id_proof.grid(row=8, column=0, sticky=W)
+
+        combo_id_proof = ttk.Combobox(label_frame_left, font=("TkDefaultFont", 10, "bold"), width=8, state="readonly")
+        combo_id_proof["value"] = ("Identity Card", "Passport", "Driving Licence")
+        combo_id_proof.current(0)
+        combo_id_proof.grid(row=8, column=1)
+
+        # id number
+        id_number = Label(label_frame_left, text="ID Number", font=("TkDefaultFont", 10, "bold"), padx=2, pady=6)
+        id_number.grid(row=9, column=0, sticky=W)
+
+        entry_id_number = ttk.Entry(label_frame_left, width=10, font=("TkDefaultFont", 10, "bold"))
+        entry_id_number.grid(row=9, column=1)
+
+        # Adress
+        adress = Label(label_frame_left, text="Address", font=("TkDefaultFont", 10, "bold"), padx=2, pady=6)
+        adress.grid(row=10, column=0, sticky=W)
+
+        entry_adress = ttk.Entry(label_frame_left, width=10, font=("TkDefaultFont", 10, "bold"))
+        entry_adress.grid(row=10, column=1)
+
+        # =================== buttons ===================
+        button_frame = Frame(label_frame_left, bd=2, relief=RIDGE,)
+        button_frame.place(x=0, y=370)
+
+        button_add = Button(button_frame, text="Add", font=("TkDefaultFont", 10, "bold"), bg= "dark blue", fg="white", width=6)
+        button_add.grid(row=0, column=0,)
+
+        button_update = Button(button_frame, text="Update", font=("TkDefaultFont", 10, "bold"), bg= "dark blue", fg="white", width=6)
+        button_update.grid(row=0, column=1, padx=1)
+
+        button_delete = Button(button_frame, text="Delete", font=("TkDefaultFont", 10, "bold"), bg= "dark blue", fg="white", width=6)
+        button_delete.grid(row=0, column=2, padx=1)
+
+        button_reset = Button(button_frame, text="Reset", font=("TkDefaultFont", 10, "bold"), bg= "dark blue", fg="white", width=6)
+        button_reset.grid(row=0, column=3, padx=1)
+
+        # =================== table frame ===================
+        table_frame = LabelFrame(self.root, text="View Details and Search System", font=("TkDefaultFont", 10, "bold"), bd=4, relief=RIDGE)
+        table_frame.place(x=255, y=50, width=860, height=450)
+
+        searchby = Label(table_frame, text="Search By", font=("TkDefaultFont", 10, "bold"), bg="red", fg="white")
+        searchby.grid(row=0, column=0, sticky=W, padx=2)
+        combo_id_search = ttk.Combobox(table_frame, font=("TkDefaultFont", 10, "bold"), width=14, state="readonly")
+        combo_id_search["value"] = ("Mobile", "Ref",)
+        combo_id_search.current(0)
+        combo_id_search.grid(row=0, column=1, padx=2)
+
+        search = ttk.Entry(table_frame, width=14, font=("TkDefaultFont", 10, "bold"))
+        search.grid(row=0, column=2, padx=2)
+
+        button_search = Button(table_frame, text="Search", font=("TkDefaultFont", 10, "bold"), bg= "dark blue", fg="white", width=7)
+        button_search.grid(row=0, column=3, padx=2)
+
+        button_showall = Button(table_frame, text="Show All", font=("TkDefaultFont", 10, "bold"), bg= "dark blue", fg="white", width=7)
+        button_showall.grid(row=0, column=4, padx=2)
+
+        # =================== show data table ===================
+        details_table = Frame(table_frame, bd=2, relief=RIDGE,)
+        details_table.place(x=0, y=50, width=860, height=350)
+
+        scroll_x = ttk.Scrollbar(details_table, orient=HORIZONTAL)
+        scroll_y = ttk.Scrollbar(details_table, orient=VERTICAL)
+
+        self.customer_details_table = ttk.Treeview(details_table, column=("Ref", "Name", "Mother", "Gender", "Postcode", "Mobile", "Email", "Nationality", "Id Proof", "Id Number", "Address"), xscrollcommand=scroll_x.set, yscrollcommand=scroll_y.set,)
+        
+        scroll_x.pack(side=BOTTOM, fill=X)
+        scroll_y.pack(side=RIGHT, fill=Y)
+
+        scroll_x.config(command=self.customer_details_table.xview)
+        scroll_y.config(command=self.customer_details_table.yview)
+
+        self.customer_details_table.heading("Ref", text="Refer No")
+        self.customer_details_table.heading("Name", text="Name")
+        self.customer_details_table.heading("Mother", text="Mother")
+        self.customer_details_table.heading("Gender", text="Gender")
+        self.customer_details_table.heading("Postcode", text="Postcode")
+        self.customer_details_table.heading("Mobile", text="Mobile")
+        self.customer_details_table.heading("Email", text="Email")
+        self.customer_details_table.heading("Nationality", text="Nationality")
+        self.customer_details_table.heading("Id Proof", text="Id Proof")
+        self.customer_details_table.heading("Id Number", text="Id Number")
+        self.customer_details_table.heading("Address", text="Address")
+
+        self.customer_details_table["show"]="headings"
+
+        self.customer_details_table.column("Ref", width=100)
+        self.customer_details_table.column("Name", width=100)
+        self.customer_details_table.column("Mother", width=100)
+        self.customer_details_table.column("Gender", width=100)
+        self.customer_details_table.column("Postcode", width=100)
+        self.customer_details_table.column("Mobile", width=100)
+        self.customer_details_table.column("Email", width=100)
+        self.customer_details_table.column("Nationality", width=100)
+        self.customer_details_table.column("Id Proof", width=100)
+        self.customer_details_table.column("Id Number", width=100)
+        self.customer_details_table.column("Address", width=100)
+
+        self.customer_details_table.pack(fill=BOTH, expand=1)
+
+if __name__ == "__main__":
+    root = Tk()
+    obj = Custom_Window(root)
+    root.mainloop()
--- hotel.py
+++ hotel.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from PIL import Image, ImageTk 
+from customer import Custom_Window
 
 class HotelManagementSystem:
     def __init__(self, root):
@@ -14,6 +15,7 @@ class HotelManagementSystem:
         labelimg = Label(self.root, image = self.photoimg1, bd = 4, relief = RIDGE)
         labelimg.place(x = 250, y = 0, width= 1700, height= 200)
 
+        # ================= logo =====================
         img2 = Image.open("C:\\Users\\Nazife\\Desktop\\hotel_management_system\\images\\hotel_logo.jpg")
         img2 = img2.resize((250, 200))
         self.photoimg2 = ImageTk.PhotoImage(img2)
@@ -21,20 +23,23 @@ class HotelManagementSystem:
         labelimg = Label(self.root, image = self.photoimg2, bd = 4, relief = RIDGE)
         labelimg.place(x = 0, y = 0, width= 250, height= 200)
 
+        # ================= title =====================
         label_title = Label(self.root, text="HOTEL MANAGEMENT SYSTEM", justify="center", font=("TkDefaultFont", 40, "bold"), bg= "dark blue", fg="white", bd=4, relief=RIDGE,)
         label_title.place(x=0, y=200, width=1400, height=50)
 
+        # ================= main frame =====================
         main_frame = Frame(self.root, bd=4, relief=RIDGE)
         main_frame.place(x=0, y= 250, width=1400, height=620)
 
-
+        # ================= menu =====================
         label_menu = Label(main_frame, text="MENU", justify="center", font=("TkDefaultFont", 20, "bold"), bg= "dark blue", fg="white", bd=4, relief=RIDGE,)
         label_menu.place(x=0, y=0, width=250,)
 
+        # =================button frame=====================
         button_frame = Frame(main_frame, bd=4, relief=RIDGE)
         button_frame.place(x=0, y= 35, width=250, height=190)
 
-        customer_btn = Button(button_frame, text="CUSTOMER", justify="center", width=20, font=("TkDefaultFont", 14, "bold"), bg= "dark blue", fg="white", bd=0, cursor="hand2")
+        customer_btn = Button(button_frame, text="CUSTOMER", command=self.custom_details, justify="center", width=20, font=("TkDefaultFont", 14, "bold"), bg= "dark blue", fg="white", bd=0, cursor="hand2")
         customer_btn.grid(row=0, column=0, pady=1)
 
         room_btn = Button(button_frame, text="ROOM", justify="center", width=20, font=("TkDefaultFont", 14, "bold"), bg= "dark blue", fg="white", bd=0, cursor="hand2")
@@ -48,6 +53,11 @@ class HotelManagementSystem:
 
         logout_btn = Button(button_frame, text="LOGOUT", justify="center", width=20, font=("TkDefaultFont", 14, "bold"), bg= "dark blue", fg="white", bd=0, cursor="hand2")
         logout_btn.grid(row=4, column=0, pady=1)
+
+    
+    def custom_details(self):
+        self.new_window = Toplevel(self.root)
+        self.app = Custom_Window(self.new_window)
 
 
 if __name__ == "__main__":
